4b4c71c7060f763986ea0f7e6f828c5678b10341
Dalton P. Franco <dpacola.dev@gmail.com>
2025-03-06 15:56:20 -0300

    Add Edit Options in Menu

--- src/Main.java
+++ src/Main.java
@@ -19,8 +19,9 @@ public class Main {
                 """
                     --- MENU ---
                     [1] - Create a new Ninja!
-                    [2] - List all ninjas registered in Array
-                    [3] - Delete a ninja registered in Array
+                    [2] - List all ninjas registered in Array.
+                    [3] - Delete a ninja registered in Array.
+                    [4] - Edit a ninja registered in Array.
                     [0] - Exit of the Program!
                 """
             );
@@ -124,7 +125,7 @@ public class Main {
                     listArrayOfNinjas(ninjas);
 
                     while (true) {
-                        System.out.print("Choose a index of the Array of Ninjas for create a new Ninja: ");
+                        System.out.print("Choose a index of the Array of Ninjas for delete a Ninja: ");
                         chosenNinjaIndex = scanner.nextInt();
                         scanner.nextLine();
 
@@ -147,8 +148,80 @@ public class Main {
 
                     break;
 
+                case 4:
+                    if (amountOfNinjasInArrayOfNinjas == 0) {
+                        System.out.println("Nothing Ninjas in Array of Ninjas!");
+                        System.out.println();
+                        continue;
+                    }
+
+                    listArrayOfNinjas(ninjas);
+
+                    while (true) {
+                        System.out.print("Choose a index of the Array of Ninjas for edit a Ninja: ");
+                        chosenNinjaIndex = scanner.nextInt();
+                        scanner.nextLine();
+
+                        if (chosenNinjaIndex < 1 || chosenNinjaIndex > sizeOfArrayOfNinjas) {
+                            System.out.printf("The index: %d is not valid! Retry Again!\n", chosenNinjaIndex);
+                            continue;
+                        }
+
+                        if (ninjas[chosenNinjaIndex-1] == null) {
+                            System.out.printf("The index: %d haven't a ninja. Retry Again\n", chosenNinjaIndex);
+                            continue;
+                        }
+
+                        break;
+                    }
+
+                    while (true) {
+                        System.out.print("What's name of the new Ninja: ");
+                        String nameNewNinja = scanner.nextLine();
+
+                        if (nameNewNinja.isEmpty()) {
+                            System.out.println("The name of the Ninja can't empty! Retry Again!");
+                            System.out.println();
+                            continue;
+                        }
+
+                        System.out.print("What's age of the new Ninja: ");
+
+                        if (!scanner.hasNextInt()) {
+                            System.out.println("You enter a invalid Age for new Ninja, Retry Again!");
+                            scanner.nextLine();
+                            System.out.println();
+                            continue;
+                        }
+
+                        int ageNewNinja = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.print("What's village of the new Ninja: ");
+                        String villageNewNinja = scanner.nextLine();
+
+                        if (villageNewNinja.isEmpty()) {
+                            System.out.println("The village of the Ninja can't empty! Retry Again!");
+                            System.out.println();
+                            continue;
+                        }
+
+                        Ninja ninjaEdit = ninjas[chosenNinjaIndex-1];
+                        ninjaEdit.setName(nameNewNinja);
+                        ninjaEdit.setAge(ageNewNinja);
+                        ninjaEdit.setVillage(villageNewNinja);
+                        System.out.println();
+
+                        break;
+                    }
+                    break;
+
                 case 0:
                     System.out.println("Bye Bye...");
+                    break;
+
+                default:
+                    System.out.printf("The item: %d is not valid! Retry Again!\n", chosenMenuItem);
                     break;
             }
         }
